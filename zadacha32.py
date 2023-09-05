eb8f3975b5bf02e991b02f5c3ac1ffd1602e34a0
VarentsovaMV <varentsovamv@gmail.com>
2023-09-05 16:42:33 +0300

    Исправлена задача 32

--- zadacha32.py
+++ zadacha32.py
@@ -5,10 +5,11 @@ from random import randint
 n = int(input("введите количество элементов списка: "))
 list1 = [randint(0, 20) for i in range(n)]
 print(list1)
-min = int(input("введите минимальное значение: "))
-max = int(input("введите максимальное значение: "))
+minimum = int(input("введите минимальное значение: "))
+maximum = int(input("введите максимальное значение: "))
 list2 = []
 for i in range(len(list1)):
-    if list1[i] > min and list1[i] < max:
-        list2.append(i)
+    if list1[i] > minimum:
+        if list1[i] < maximum:
+            list2.append(i)
 print(list2)
